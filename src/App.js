148e0617f35afe2e72a11627cf5d63e5bb864158
jordelucas <jordevalucas@hotmail.com>
2020-02-05 15:41:00 -0300

    add Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hunt Web</h1>
-    </div>
-  );
-}
+import "./styles.css"
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
 
 export default App;
